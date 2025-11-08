c49941af292cd736d5faf3b5b56c6610fb1cdf5a
clarioncollege <emmanueltosin789@gmail.com>
2025-11-08 04:14:39 +0100

    Update CounterButton.jsx

--- src/components/CounterButton.jsx
+++ src/components/CounterButton.jsx
@@ -1,6 +1,6 @@
 import styles from "./CounterButton.module.css";
 function CounterButton() {
-  return <button className={styles.btn}>count is</button>;
+  return <button className={styles.btn}>Increase Count</button>;
 }
 
 export default CounterButton;
